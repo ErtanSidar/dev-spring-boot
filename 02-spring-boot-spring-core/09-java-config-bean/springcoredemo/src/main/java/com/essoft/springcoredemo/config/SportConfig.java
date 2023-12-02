88f1b9ea8064036a76e174763695f08a5346598b
Ertan SİDAR <essoft3@gmail.com>
2023-12-02 15:11:41 +0300

    java config bean

---
+++ 02-spring-boot-spring-core/09-java-config-bean/springcoredemo/src/main/java/com/essoft/springcoredemo/config/SportConfig.java
@@ -0,0 +1,14 @@
+package com.essoft.springcoredemo.config;
+
+import com.essoft.springcoredemo.common.Coach;
+import com.essoft.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+    @Bean("aquatic")
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
+}
---
+++ 02-spring-boot-spring-core/09-java-config-bean/springcoredemo/src/main/java/com/essoft/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.essoft.springcoredemo.rest;
+
+import com.essoft.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach coach;
+
+    @Autowired
+    public DemoController(@Qualifier("aquatic") Coach coach) {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.coach.getDailyWorkout();
+    }
+
+}
